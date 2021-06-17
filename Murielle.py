65d82e76d809c2c3304c2a3d86654fbe057d84f1
Lynnexxx <75989044+Lynnexxx@users.noreply.github.com>
2021-06-17 15:09:56 +0100

    Add files via upload

--- Murielle.py
+++ Murielle.py
@@ -1,9 +1,9 @@
-Murielle = {'SDA':[15,16,18], 'Java':[16,15,18], 'Statistique':[16,15,18], 'Reseau': [16,15,18], 'UML':[16,15,18], 'Sys_d'Information':[16,15,18]}
+Murielle = {'SDA':[15,16,18], 'Java':[16,15,18], 'Statistique':[16,15,18], 'Reseau': [16,15,18], 'UML':[16,15,18], 'Sys_Information':[16,15,18]}
 
 s1 = (Murielle['SDA'][0] + Murielle['SDA'][1] + Murielle['SDA'][2])/3
 s2 = (Murielle['Java'][0] + Murielle['Java'][1] + Murielle['Java'][2])/3
 s3 = (Murielle['Statistique'][0] + Murielle['Statistique'][1] + Murielle['Statistique'][2])/3
-s4 = (Murielle['Sys_d'Information'][0] + Murielle['Sys_d'Information'][1] + Murielle['Sys_d'Information'][2])/3
+s4 = (Murielle['Sys_Information'][0] + Murielle['Sys_Information'][1] + Murielle['Sys_Information'][2])/3
 s5=  (Murielle['Reseau'][0] + Murielle['Reseau'][1] + Murielle['Reseau'][2])/3
 s6=  (Murielle['UML'][0] + Murielle['UML'][1] + Murielle['UML'][2])/3
 
