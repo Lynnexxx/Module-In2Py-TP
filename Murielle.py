9626d21fe4a36ca884cd5f15a41f9cd30cf5b79d
Lynnexxx <75989044+Lynnexxx@users.noreply.github.com>
2021-06-17 15:42:04 +0100

    Add files via upload

--- Murielle.py
+++ Murielle.py
@@ -1,4 +1,7 @@
-Murielle = {'SDA':[15,16,18], 'Java':[16,15,18], 'Statistique':[16,15,18], 'Reseau': [16,15,18], 'UML':[16,15,18], 'Sys_Information':[16,15,18]}
+print( '---------------------------EXERCICE Moyenne-------------------------------------------')
+
+
+Murielle = {'SDA':[15,16,18], 'Java':[16,15,18], 'Statistique':[16,14,18], 'Reseau': [14,15,16], 'UML':[13,16,17], 'Sys_Information':[16,15,18]}
 
 s1 = (Murielle['SDA'][0] + Murielle['SDA'][1] + Murielle['SDA'][2])/3
 s2 = (Murielle['Java'][0] + Murielle['Java'][1] + Murielle['Java'][2])/3
@@ -9,3 +12,44 @@ s6=  (Murielle['UML'][0] + Murielle['UML'][1] + Murielle['UML'][2])/3
 
 
 print ((s1+s2+s3+s4+s5+s6)/6)
+
+
+print( '---------------------------EXERCICE E0-------------------------------------------')
+
+l =list(range(1,11))
+print (l)
+
+print('----------------------------------------------------------')
+
+l[0] = l[0]+11
+l[1] = l[1]+11
+l[2] = l[2]+11
+l[3] = l[3]+11
+l[4] = l[4]+11
+l[5] = l[5]+11
+l[6] = l[6]+11
+l[7] = l[7]+11
+l[8] = l[8]+11
+l[9] = l[9]+11
+
+print(list(l))
+
+
+print('----------------------------------------------------------')
+
+l.append(22)
+print(list(l))
+print('----------------------------------------------------------')
+
+
+l.extend([23,24])
+print(list(l))
+print('----------------------------------------------------------')
+print("la sous-liste de nombres impairs")
+print(l[1:13 :2])
+print("la sous-liste de nombres pairs")
+print(l[0:13 :2])
+print('----------------------------------------------------------')
+
+l.remove(15)
+print(l)
